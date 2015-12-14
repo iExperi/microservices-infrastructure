6db2cc2b62923daf1b8750c04d787323a94698b1
Sam Hatfield <samuel.e.hatfield@gmail.com>
2015-12-14 14:51:24 -0600

    Modified OS credentials to point to CC tx3

--- testing/openstack.tf
+++ testing/openstack.tf
@@ -1,18 +1,18 @@
 module "mantl-ci-keypair" {
   source = "./terraform/openstack/keypair"
-  auth_url = "https://eu-amsterdam-1.cloud.cisco.com:5000/v2.0"
-  tenant_id = "55c1ad1a57064bc89b59e8bb117e6ece"
-  tenant_name = "shipped-eu"
+  auth_url = "https://us-texas-3.cloud.cisco.com:5000/v2.0"
+  tenant_id = "239238473d674e138ac07bc5c9b0a6d5"
+  tenant_name = "mantl-tx-3"
   public_key = "~/.ssh/id_rsa.pub"
   keypair_name = "mantl-ci"
 }
 
 module "mantl-ci-hosts-floating" {
   source = "./terraform/openstack/hosts-floating"
-  auth_url = "https://eu-amsterdam-1.cloud.cisco.com:5000/v2.0"
+  auth_url = "https://us-texas-3.cloud.cisco.com:5000/v2.0"
   datacenter = "mantl-ci"
-  tenant_id = "55c1ad1a57064bc89b59e8bb117e6ece"
-  tenant_name = "shipped-eu"
+  tenant_id = "239238473d674e138ac07bc5c9b0a6d5"
+  tenant_name = "mantl-tx-3"
   control_flavor_name = "CO2-Medium"
   worker_flavor_name  = "CO2-Medium"
   edge_flavor_name  = "CO2-Medium"
