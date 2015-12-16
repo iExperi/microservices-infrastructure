ff3c357b62c468dcadf1a0c781b8534a09fefe11
Sam Hatfield <samuel.e.hatfield@gmail.com>
2015-12-16 16:51:17 -0600

    Adding GCE, including the accounts.json file contents in .drone.sec

---
+++ testing/gce.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  credentials = "${file("account.json")}"
+  project = "mantl-ci"
+  region = "us-central1"
+}
+
+module "gce-dc" {
+  source = "./terraform/gce"
+  datacenter = "gce-dc"
+  control_type = "n1-standard-1"
+  worker_type = "n1-highcpu-2"
+  network_ipv4 = "10.0.0.0/16"
+  long_name = "mantl-drone-ci"
+  short_name = "drone-ci"
+  region = "us-central1"
+  zone = "us-central1-a"
+  control_count = 3
+  worker_count = 3
+  edge_count = 2
+}
